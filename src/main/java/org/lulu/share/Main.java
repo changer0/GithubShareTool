5d96e782521c8a5ba1d374a8e5cb538f935be1a9
zhanglulu <zhanglulu1007@foxmail.com>
2022-05-25 22:29:05 +0800

    3

--- src/main/java/org/lulu/share/Main.java
+++ src/main/java/org/lulu/share/Main.java
@@ -2,7 +2,13 @@ package org.lulu.share;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.errors.StopWalkException;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.revwalk.filter.RevFilter;
 import org.eclipse.jgit.transport.CredentialsProvider;
 
 import java.io.File;
@@ -19,9 +25,11 @@ public class Main {
 //        KVStorage.put("un", "123");
 //        KVStorage.put("pwd", "123");
 //        new GitShareFrame();
-        System.out.println("url: " + git.getRepository().getConfig().getString("remote", "origin", "url"));
+        Repository repository = git.getRepository();
+        String url = repository.getConfig().getString("remote", "origin", "url").replace(".git", "");
+        System.out.println("url: " + url);
         System.out.println(git);
-
-
+        boolean isPushed = repository.resolve("origin/master").equals(repository.resolve("master"));
+        System.out.println(isPushed);
     }
 }
